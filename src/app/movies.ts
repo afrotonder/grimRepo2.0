13269a16d1e441fe549f82c8a646c25b06ccf151
afrotonder <o.rosado18@gmail.com>
2020-02-18 14:53:56 -0400

    Initial commit

---
+++ src/app/movies.ts
@@ -0,0 +1,35 @@
+export const movies = [
+  {
+    title: 'Halloween',
+    year: 1978,
+    image: 'https://images-na.ssl-images-amazon.com/images/I/41MMA2HHB1L._AC_.jpg'
+  },
+  {
+    title: 'PumpkinHead',
+    year: 1988,
+    image: 'https://i.pinimg.com/originals/bf/25/8c/bf258c57954412ad66fc40dadedce6e3.jpg'
+  },
+  {
+    title: 'The Texas Chainsaw Massacre',
+    year: 1974,
+    image: 'https://doctorgash.files.wordpress.com/2011/02/tcm.jpg'
+  },
+  {
+    title: 'Salems Lot',
+    year: 1995,
+    image: 'https://images-na.ssl-images-amazon.com/images/I/418DYHFY7QL._AC_SY445_.jpg'
+  },
+  // {
+  //   title: '',
+  //   year: ,
+  //   image: ''
+  // },
+
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
+import { movies } from '../movies';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss']
+})
+export class ProductListComponent {
+  movies = movies;
+
+    closeResult: string;
+
+  constructor(private modalService: NgbModal) {}
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+   private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+
+
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
