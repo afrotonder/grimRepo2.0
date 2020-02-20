6b410127a9ff0bf1e5e3925d6d7e36d06a9496bc
afrotonder <o.rosado18@gmail.com>
2020-02-20 14:13:30 -0400

    Add json file with movie obj & add some dependencies ;

--- src/app/films/films.component.ts
+++ src/app/films/films.component.ts
@@ -5,6 +5,8 @@ import { FormBuilder } from '@angular/forms';
 
 //@ts-ignore // why i have to do this is beyond me 
 import { movies } from '../movies';
+import  movie  from '../movies.json';
+
 
 @Component({
   selector: 'app-films',
@@ -33,7 +35,7 @@ export class FilmsComponent  {
 
   open(content) {
 
-    console.log("wo ", this.films)
+    console.log("wo ",movie)
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
@@ -50,12 +52,17 @@ export class FilmsComponent  {
     this.newFilm = customerData ;
     console.log("new film data ", this.newFilm) 
     console.log("this filmsd ", movies)
-    movies.push(this.newFilm)
+    // movie.push(this.newFilm)
     this.addFilm.reset();
 
     console.warn('Your order has been submitted', customerData);
+    
+    const writeJsonFile = require('write-json-file');
+ 
+      (async () => {
+          await writeJsonFile('foo.json', {foo: true});
+      })();
 
-   localStorage.setItem('movies', JSON.stringify(movies));
 
 
   }
