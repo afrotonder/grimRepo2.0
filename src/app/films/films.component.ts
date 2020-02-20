cb6c23678db8d0710b815e3c2bf935916a188c1c
afrotonder <o.rosado18@gmail.com>
2020-02-20 14:23:34 -0400

    comment json ref ;

--- src/app/films/films.component.ts
+++ src/app/films/films.component.ts
@@ -5,7 +5,7 @@ import { FormBuilder } from '@angular/forms';
 
 //@ts-ignore // why i have to do this is beyond me 
 import { movies } from '../movies';
-import  movie  from '../movies.json';
+// import  movie  from '../movies.json';
 
 
 @Component({
