28c96dfdabcdaa42e0f936ddf1aeab49137fbb3d
afrotonder <o.rosado18@gmail.com>
2020-02-20 13:53:29 -0400

    Adds woking modal with angular form. Form working & adding films but not storing yet.

--- src/app/films/films.component.ts
+++ src/app/films/films.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder } from '@angular/forms';
+
 
 //@ts-ignore // why i have to do this is beyond me 
 import { movies } from '../movies';
@@ -12,17 +14,50 @@ import { movies } from '../movies';
 export class FilmsComponent  {
 
   films = movies;
+  addFilm ;
+  newFilm ;
 
     closeResult: string;
 
-  constructor(private modalService: NgbModal) {}
+  constructor(
+                private modalService: NgbModal,
+                private formBuilder: FormBuilder
+              ) {
+
+     this.addFilm = this.formBuilder.group({
+      title: '',
+      year: '',
+      image: ''
+    });
+  }
 
   open(content) {
+
+    console.log("wo ", this.films)
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
     });
+  }
+
+    filter() {
+      console.log("filter soon!")
+  }
+
+   onSubmit(customerData) {
+    // Process checkout data here
+    this.newFilm = customerData ;
+    console.log("new film data ", this.newFilm) 
+    console.log("this filmsd ", movies)
+    movies.push(this.newFilm)
+    this.addFilm.reset();
+
+    console.warn('Your order has been submitted', customerData);
+
+   localStorage.setItem('movies', JSON.stringify(movies));
+
+
   }
 
    private getDismissReason(reason: any): string {
